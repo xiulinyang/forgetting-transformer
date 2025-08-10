e153490fde17d54c054c636f4ebb85030455004b
xiulinyang <xiulin.yang.compling@gmail.com>
2025-08-09 20:36:50 -0400

    [BUG] remove unnessary arguments in datamodule

--- configs/datamodule/npy.py
+++ configs/datamodule/npy.py
@@ -9,13 +9,11 @@ class NPYDataModuleConfig(DataModuleConfig):
     data_path: str = "${data_dir}"
     rank: int = MISSING
     world_size: int = MISSING
-    train_seq_len: Optional[int] = None
     train_batch_len: int = MISSING
     train_batch_size: int = MISSING
     train_doc_len: Optional[int] = None
     train_num_workers: int = MISSING
     eval_tokens: int = MISSING
-    eval_seq_len: Optional[int] = None
     eval_batch_len: int = MISSING
     eval_local_batch_size: int = MISSING
     eval_doc_len: Optional[int] = None
