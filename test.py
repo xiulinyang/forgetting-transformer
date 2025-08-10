7b2d7e285799acd1510a58c98ae68209755e8613
xiulinyang <xiulin.yang.compling@gmail.com>
2025-08-10 18:18:10 +0000

    update test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 
-data = np.load("/Users/xiulinyang/Downloads/batch_0_to_1000.npy")  # don't forget .npy
+data = np.load("data/train.npy")  # don't forget .npy
 num_rows = data.shape[0]
 num_eval = int(num_rows * 0.03)
 
@@ -9,6 +9,6 @@ rows_sample = random.sample(range(num_rows), num_eval)
 
 eval_data = data[rows_sample]
 
-np.save("eval.npy", eval_data)
+np.save("data/eval.npy", eval_data)
 
 print(f"Eval shape: {eval_data.shape}")
