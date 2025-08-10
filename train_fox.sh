3d21fae4978f0396aaf7c9fe060eae56f3af4ae8
xiulinyang <xiulin.yang.compling@gmail.com>
2025-08-10 02:30:31 +0200

    update config setting

--- train_fox.sh
+++ train_fox.sh
@@ -4,18 +4,18 @@ OUTPUT_DIR="./forgetting_gate/"  # You can set this to any other path
 WANDB_DIR="./output/wandb"  # You can set this to any other path
 mkdir -p $OUTPUT_DIR
 mkdir -p $WANDB_DIR
-fabric run train.py \
-    --devices 4 \
-    --num-nodes 1 \
-    --node-rank 0 \
-    --main-address localhost \
-    --main-port 1234 \
-    +experiment/pile/forgetting_transformer=forgetting_gate \
-    seed=42 \
-    exp=forgetting_gate_exp \
-    tag=ft_l2_h256_h4_seq2048_adam \
-    output_dir=$OUTPUT_DIR \
-    data_dir=$DATA_DIR \
-    wandb.log_dir=$WANDB_DIR \
-    wandb.mode=offline \
-    resume=true
+
+torchrun --nproc_per_node=4 --master_port=1234 train.py \
+  +experiment/pile/forgetting_transformer=forgetting_gate \
+  fabric.devices=4 \
+  fabric.precision=16-mixed \
+  seed=42 \
+  exp=forgetting_gate_exp \
+  tag=ft_l2_h256_h4_seq2048_adam \
+  output_dir=./forgetting_gate/ \
+  data_dir=$DATA_DIR \
+  wandb.log_dir=./output/wandb \
+  wandb.mode=online \
+  resume=false
+
+
